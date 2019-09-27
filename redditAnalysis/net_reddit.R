32651d730aa174887ee1dc5217e7ccd41f2bebca
andrés castro araújo <acastroaraujo@gmail.com>
2019-09-27 16:30:47 -0400

    update

--- redditAnalysis/net_reddit.R
+++ redditAnalysis/net_reddit.R
@@ -12,31 +12,9 @@ net_grow_tree <- function(nodes, edges) {
 # **********************************************************
 # Use purrr::map2 to use this function in a dplyr pipeline:
 # df <- df %>% 
+#   filter(purrr::map_lgl(edges, is.null)) %>% 
 #   mutate(graph = purrr::map2(nodes, edges, net_grow_tree))
 # **********************************************************
-
-net_grow_tree(df$node_data[[1]], df$edge_list[[1]])
-
-
-df <- df %>% 
-  filter(!map_lgl(edge_list, is.null)) 
-  mutate(graph = purrr::map2(node_data, edge_list, net_grow_tree))
-  
-  
-
-net_plot <- function(graph, layout = "circlepack", circular = TRUE, title = "") {
-  require(ggraph)
-  
-  graph %>% 
-    ggraph(layout, circular = circular) +
-    geom_edge_diagonal(alpha = 0.5, width = 0.1) +
-    geom_node_point(size = 0.01) +
-    theme_graph() +
-    labs(title = str_wrap(title, width = 20))
-  
-}
-
-
 
 net_plot <- function(
   graph,
@@ -87,11 +65,10 @@ net_plot <- function(
     
   }
   
-  g + 
-    geom_node_label(aes(filter = is.na(score) | rank(-descendents) <= n_label_rank, 
-                        label = str_wrap(str_trunc(text, width = 300), width = 50),
-                        color = root), size = 2, repel = TRUE, alpha = 0.9, 
-                    show.legend = FALSE) +
+  g + geom_node_label(
+    aes(filter = is.na(score) | rank(-descendents) <= n_label_rank, 
+        label = str_wrap(str_trunc(text, width = 300), width = 50), color = root), 
+    size = 2, repel = TRUE, alpha = 0.9, show.legend = FALSE) +
     scale_color_manual(values = c("black", "dodgerblue")) +
     theme_graph() +
     labs(title = str_wrap(title, width = 50), caption = url) +
