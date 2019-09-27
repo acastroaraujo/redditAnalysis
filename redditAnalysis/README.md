150dcdf64db12dbce4c4e8292727de99b10128dd
andrés castro araújo <acastroaraujo@gmail.com>
2019-09-27 16:23:01 -0400

    added network functions

--- redditAnalysis/README.md
+++ redditAnalysis/README.md
@@ -1,1 +1,9 @@
-The functions withe a `w2v_` prefix are designed to fitting and analyzing word2vec models around Reddit conversations.
+The functions collected here start with prefix that's supposed to be indicative of the types of analysis we do.
+
+- `w2v_`: functions for fitting and analyzing word2vec models around Reddit conversations.
+
+- `net_`: functions for analyzing with the network structure of reddit conversations.
+
+    For example, this is one output of `net_plot()`
+
+    ![](net_plot.png)
---
+++ redditAnalysis/net_reddit.R
@@ -0,0 +1,101 @@
+
+# *********************************************************
+# This script contains functions for working with word2vec
+# models trained on Reddit conversations.
+# - net_plot
+# *********************************************************
+
+net_grow_tree <- function(nodes, edges) {
+  igraph::graph_from_data_frame(d = edges, vertices = nodes, directed = TRUE)
+}
+
+# **********************************************************
+# Use purrr::map2 to use this function in a dplyr pipeline:
+# df <- df %>% 
+#   mutate(graph = purrr::map2(nodes, edges, net_grow_tree))
+# **********************************************************
+
+net_grow_tree(df$node_data[[1]], df$edge_list[[1]])
+
+
+df <- df %>% 
+  filter(!map_lgl(edge_list, is.null)) 
+  mutate(graph = purrr::map2(node_data, edge_list, net_grow_tree))
+  
+  
+
+net_plot <- function(graph, layout = "circlepack", circular = TRUE, title = "") {
+  require(ggraph)
+  
+  graph %>% 
+    ggraph(layout, circular = circular) +
+    geom_edge_diagonal(alpha = 0.5, width = 0.1) +
+    geom_node_point(size = 0.01) +
+    theme_graph() +
+    labs(title = str_wrap(title, width = 20))
+  
+}
+
+
+
+net_plot <- function(
+  graph,
+  title = "",
+  url = "",
+  style = 1,
+  n_label_rank = 5
+  ) {
+  
+  stopifnot(style %in% 1:4)
+  
+  if (style == 1) {
+    message("circle pack!")
+    g <- tidygraph::as_tbl_graph(graph) %>% 
+      mutate(root = ifelse(is.na(score), TRUE, FALSE)) %>%
+      ggraph("circlepack", circular = TRUE) +
+      geom_edge_link(alpha = 0.5, width = 0.1, check_overlap = TRUE) +
+      geom_node_point(size = 0.01)
+    
+  }
+  
+  if (style == 2) {
+    message("curvy circle pack!")
+    g <- tidygraph::as_tbl_graph(graph) %>% 
+      mutate(root = ifelse(is.na(score), TRUE, FALSE)) %>%
+      ggraph("circlepack", circular = TRUE) +
+      geom_edge_arc(alpha = 0.5, width = 0.1, check_overlap = TRUE, strength = 1/4) +
+      geom_node_point(size = 0.01)
+    
+  }
+  
+  if (style == 3) {
+    message("dendrogram!")
+    g <- tidygraph::as_tbl_graph(graph) %>% 
+      mutate(root = ifelse(is.na(score), TRUE, FALSE)) %>%
+      ggraph("dendrogram", circular = FALSE) +
+      geom_edge_diagonal(alpha = 0.5, width = 0.1, check_overlap = TRUE) +
+      geom_node_point(size = 0.01)
+  }
+  
+  if (style == 4) {
+    message("circular dendrogram!")
+    g <- tidygraph::as_tbl_graph(graph) %>% 
+      mutate(root = ifelse(is.na(score), TRUE, FALSE)) %>%
+      ggraph("dendrogram", circular = TRUE) +
+      geom_edge_diagonal(alpha = 0.5, width = 0.1, check_overlap = TRUE) +
+      geom_node_point(size = 0.01)
+    
+  }
+  
+  g + 
+    geom_node_label(aes(filter = is.na(score) | rank(-descendents) <= n_label_rank, 
+                        label = str_wrap(str_trunc(text, width = 300), width = 50),
+                        color = root), size = 2, repel = TRUE, alpha = 0.9, 
+                    show.legend = FALSE) +
+    scale_color_manual(values = c("black", "dodgerblue")) +
+    theme_graph() +
+    labs(title = str_wrap(title, width = 50), caption = url) +
+    theme(plot.title = element_text(color = "dodgerblue3", face = "bold"))
+}
+
+
